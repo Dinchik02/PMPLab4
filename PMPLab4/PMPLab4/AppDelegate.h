46cd0945a6886884b7daeaf6152de1ff74c8eb4d
Diana <Dinchik@dianas-macbook-pro.local>
2015-12-27 18:51:41 +0200

    Init project; add multiple views

---
+++ PMPLab4/PMPLab4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PMPLab4
+//
+//  Created by Diana Volodchenko on 12/27/15.
+//  Copyright © 2015 Diana Volodchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
